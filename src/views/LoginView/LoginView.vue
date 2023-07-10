010afee9f47aa708d4671e0a1ff6bee9d824940b
huyn29 <nvh1892kw@gmail.com>
2023-07-10 21:17:06 +0700

    init login

---
+++ src/views/LoginView/LoginView.vue
@@ -0,0 +1,12 @@
+<template >
+    <Login></Login>
+</template>
+
+<script>
+import Login from '../../components/Login/Login.vue'
+export default {
+    components: { Login }
+}
+</script>
+
+<style></style>
